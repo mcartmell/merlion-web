1131424a39132cf03c8c98ada4578230aaa93504
Mike Cartmell <mike@mikec.me>
2013-07-07 20:34:13 +0100

    add channels to websocket handler

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,16 +1,37 @@
 var Merlion = (function($) {
-	ws = new WebSocket('ws://10.0.0.13:11111/');
-	var handleMessage = function(message) {
-		alert(message.data);
+	window.dispatch = _.clone(Backbone.Events);
+
+	var updateLobby = function() {
+		alert('wheee!');
 	};
+
+	var ws = new WebSocket('ws://10.0.0.13:11111/');
+
+	// Message handler. Channel is first element of the array, payload is the second.
+	// These are used to trigger events
+	var handleMessage = function(message) {
+		json = JSON.parse(message.data)
+		chan = json['merlion'][0];
+		data = json['merlion'][1];
+		dispatch.trigger(chan, data);
+	};
+
 	ws.onmessage = handleMessage;
-	ws.onopen = function() {
-		alert('hello');
-		ws.send('blah');
-	}
+
+	var init = function() {
+		// set up dispatch events
+		dispatch.on('list', updateLobby);
+
+		ws.onopen = function() {
+			ws.send('list');
+		}
+	};
+
 	return {
-		'ws': ws
+		'ws': ws,
+		'init': init
 	}
 })(jQuery);
 $(function() {
+	Merlion.init();
 });
