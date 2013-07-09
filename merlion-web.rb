b70369aac5f32f168af6b053524cffccf266206b
Mike Cartmell <mike@mikec.me>
2013-07-09 07:34:11 +0000

    read config from file

--- merlion-web.rb
+++ merlion-web.rb
@@ -2,16 +2,18 @@ require 'sinatra/base'
 require 'sinatra/synchrony'
 require 'sinatra/asset_pipeline'
 require 'slim'
+require 'sinatra/config_file'
+
 class MerlionWeb < Sinatra::Base
 	register Sinatra::Synchrony
 	register Sinatra::AssetPipeline
+	register Sinatra::ConfigFile
+
 	set :assets_precompile, %w(app.js app.css)
 	set :assets_css_compressor, :sass
 	set :assets_js_compressor, :uglifier
 
-	configure do
-		set :server, 'ws://peanut.mikec.me:11111'
-	end
+	config_file 'config.yml'
 
 	get '/' do
 		@server = settings.server
